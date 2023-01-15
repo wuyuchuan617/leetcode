e53ff02161fafc76648ea96de0833b89b7de6636
wuyuchuan617 <jenniferwu617@gmail.com>
2023-01-15 20:00:54 +0800

    Yu (`#2399`)

--- ThisWeek/2399.ts
+++ ThisWeek/2399.ts
@@ -1,2 +1,28 @@
 // 2399. Check Distances Between Same Letters
 // https://leetcode.com/problems/check-distances-between-same-letters/description/
+
+// ------------------------- Yu Chuan -------------------------
+// Runtime 86 ms Beats 46.15% / Memory 45.5 MB Beats 46.15%
+
+function checkDistances(s: string, distance: number[]): boolean {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  const isCorrect = distance.every((element, idx) => {
+    const singleAlph = alphabet[idx];
+
+    if (!s.includes(singleAlph)) return true;
+    const alphIndex = [];
+    s.split("").filter((item, index) => {
+      if (item === singleAlph) alphIndex.push(index);
+    });
+    return alphIndex[1] - alphIndex[0] - 1 === distance[idx];
+  });
+
+  return isCorrect;
+}
+
+// ------------------------- Jacob -------------------------
+
+// ------------------------- Eva -------------------------
+
+// ------------------------- Grace -------------------------
