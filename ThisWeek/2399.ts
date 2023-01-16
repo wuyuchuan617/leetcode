21bc708005ddf1bfac305133e0c0ab81dda33c7a
Eva1009 <tctct785@gmail.com>
2023-01-16 13:54:25 +0800

    Eva (`#2399`)

--- ThisWeek/2399.ts
+++ ThisWeek/2399.ts
@@ -24,5 +24,25 @@ function checkDistances(s: string, distance: number[]): boolean {
 // ------------------------- Jacob -------------------------
 
 // ------------------------- Eva -------------------------
-
+var maxLength = function(arr) {
+    const set = new Set(['']);
+    let max = 0;
+    let str;
+    
+    for(let i = 0; i < arr.length; i++) {
+        const values = [...set.values()];
+        
+        for(let j = 0; j < values.length; j++) {     
+            str = `${values[j]}${arr[i]}`;
+            set.add(str);
+            let newStr = [...new Set(str).values()].join('');
+            
+            if (str === newStr) {
+                max = Math.max(str.length, max);    
+            }            
+        }
+    }
+    
+    return max;
+};
 // ------------------------- Grace -------------------------
