f794929c4435c52b4ff501f02d2e28039fd91f06
Jay <jieyi840713@gmail.com>
2023-01-19 00:02:55 +0800

    Jay(`#917`)

--- ThisWeek/917.ts
+++ ThisWeek/917.ts
@@ -34,6 +34,27 @@ function reverseOnlyLetters(s: string): string {
 // ------------------------- Eva -------------------------
 
 // ------------------------- Jay -------------------------
+// Runtime 61 ms Beats 90.63% Memory 44.3 MB Beats 56.25%
+function reverseOnlyLetters(s: string): string {
+  let output = s.split("");
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (!/^[a-zA-Z]*$/.test(output[left])) {
+      left++;
+    } else if (!/^[a-zA-Z]*$/.test(output[right])) {
+      right--;
+    } else {
+      const temp = output[left];
+      output[left] = output[right];
+      output[right] = temp;
+      left++;
+      right--;
+    }
+  }
+  return output.join("");
+}
 
 // ------------------------- Grace -------------------------
 
