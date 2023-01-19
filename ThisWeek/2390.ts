7b72b96fdc76edf3d177551b413710bd62f55d54
Jay <jieyi840713@gmail.com>
2023-01-19 23:23:12 +0800

    Jay(`#2390`)

--- ThisWeek/2390.ts
+++ ThisWeek/2390.ts
@@ -38,6 +38,24 @@ var removeStars = function (s) {
 };
 
 // ------------------------- Jay -------------------------
+// Runtime 123 ms Beats 80% Memory 56.1 MB Beats 86.67%
+
+function removeStars(s: string): string {
+  const strArr = s.split("");
+  let right = s.length - 1;
+  let moveTime = 0;
+  while (right >= 0) {
+    if (strArr[right] == "*") {
+      strArr[right] = "";
+      moveTime++;
+    } else if (moveTime > 0) {
+      strArr[right] = "";
+      moveTime--;
+    }
+    right--;
+  }
+  return strArr.join("");
+}
 
 // ------------------------- Grace -------------------------
 // 2390 : Runtime 209 ms Beats 25% / Memory 63.5 MB Beats 37.5%
