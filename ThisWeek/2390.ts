0a9d45a7be18b95a10c9329bb2de52e5bd54c676
wuyuchuan617 <jenniferwu617@gmail.com>
2023-01-22 19:17:57 +0800

    Yu (`#2390`)

--- ThisWeek/2390.ts
+++ ThisWeek/2390.ts
@@ -10,3 +10,17 @@
 // ------------------------- Grace -------------------------
 
 // ------------------------- Yu Chuan -------------------------
+
+// Runtime 178 ms Beats 41.18% Memory 60.5 MB Beats 47.6%
+function removeStars(s: string): string {
+  const result: string[] = [];
+
+  s.split("").map((item) => {
+    // 如果這個 item 是 * 把 result 最後一個移除（也就是 ＊ 的上一個）
+    // 如果不是 ＊ 就把這個 item push 到 result 最後把 result join('')
+    if (item === "*") return result.pop();
+    result.push(item);
+  });
+
+  return result.join("");
+}
