495b3287007426ae39a27b8eb9887cee88594456
wuyuchuan617 <jenniferwu617@gmail.com>
2023-02-01 21:59:02 +0800

    Grace (`#2390`)

--- ThisWeek/2390.ts
+++ ThisWeek/2390.ts
@@ -40,6 +40,22 @@ var removeStars = function (s) {
 // ------------------------- Jay -------------------------
 
 // ------------------------- Grace -------------------------
+// 2390 : Runtime 209 ms Beats 25% / Memory 63.5 MB Beats 37.5%
+
+function removeStars(s: string): string {
+  const _s = [...s];
+  let res = [];
+
+  _s.forEach((e: string) => {
+    if (e === "*") {
+      if (res.length !== 0) res.pop();
+    } else {
+      res.push(e);
+    }
+  });
+
+  return res.join("");
+}
 
 // ------------------------- Yu Chuan -------------------------
 
