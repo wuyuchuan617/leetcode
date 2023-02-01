29c8c9502706be9f876a6a2d694bff7ac1232da4
wuyuchuan617 <jenniferwu617@gmail.com>
2023-02-01 21:58:55 +0800

    Grace (`#2202`)

--- ThisWeek/2202.ts
+++ ThisWeek/2202.ts
@@ -64,6 +64,17 @@ var maximumTop = function (nums, k) {
 // ------------------------- Jay -------------------------
 
 // ------------------------- Grace -------------------------
+// 2202 : Runtime 80 ms Beats 88.89% / Memory 53 MB Beats 77.78%
+function maximumTop(nums: number[], k: number): number {
+  const size = nums.length;
+  let max = k >= size ? 0 : nums[k];
+
+  if (size === 1 && k % 2 !== 0) return -1;
+  for (let i = 0; i < Math.min(size, k - 1); i++) {
+    max = Math.max(max, nums[i]);
+  }
+  return max;
+}
 
 // ------------------------- Yu Chuan -------------------------
 // Runtime 70 ms Beats 100% / Memory 53.8 MB Beats 50%
